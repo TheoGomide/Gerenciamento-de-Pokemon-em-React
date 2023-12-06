45ae97a450200f7b0ffba57728e81744fd81eb32
lucasfm20 <138518985+lucasfm20@users.noreply.github.com>
2023-12-06 18:51:04 -0300

    Update Computer.jsx

--- src/Routes/Computer.jsx
+++ src/Routes/Computer.jsx
@@ -17,35 +17,35 @@ function Computer() {
         <body>
           <div className="main-div">
             <div className='left'>
-                
+              <p>Colocar os pokemon aqui </p>
             </div>
            <div className="right">
                 <h1 className="box">Box 1</h1>
                 <table border={1} >
                     <th><img src="../src/sprites/charizard.jpeg" width={50} alt="cha" /></th>
-                    <th><img src="../src/sprites/charizard.jpeg" width={50} alt="cha" /></th>
-                    <th><img src="../src/sprites/charizard.jpeg" width={50} alt="cha" /></th>
-                    <th><img src="../src/sprites/charizard.jpeg" width={50} alt="cha" />
+                    <th><img src="../src/sprites/pikachu.png" width={50} alt="cha" /></th>
+                    <th><img src="../src/sprites/bubasaur.png" width={50} alt="cha" /></th>
+                    <th><img src="../src/sprites/Squirtle.png" width={50} alt="cha" />
                     </th>
 
                     <tr>
-                        <td><img src="../src/sprites/charizard.jpeg" width={50} alt="cha" /></td>
-                        <td><img src="../src/sprites/charizard.jpeg" width={50} alt="cha" /></td>
-                        <td><img src="../src/sprites/charizard.jpeg" width={50} alt="cha" /></td>
-                        <td><img src="../src/sprites/charizard.jpeg" width={50} alt="cha" /></td>
+                        <td><img src="../src/sprites/meow.png" width={50} alt="cha" /></td>
+                        <td><img src="../src/sprites/Psyduck.png" width={50} alt="cha" /></td>
+                        <td><img src="../src/sprites/Geodude.png" width={50} alt="cha" /></td>
+                        <td><img src="../src/sprites/Eevee.png" width={50} alt="cha" /></td>
                 
                     </tr>
-                    <th><img src="../src/sprites/charizard.jpeg" width={50} alt="cha" /></th>
-                    <th><img src="../src/sprites/charizard.jpeg" width={50} alt="cha" /></th>
-                    <th><img src="../src/sprites/charizard.jpeg" width={50} alt="cha" /></th>
-                    <th><img src="../src/sprites/charizard.jpeg" width={50} alt="cha" />
+                    <th><img src="../src/sprites/Snorlax.png" width={50} alt="cha" /></th>
+                    <th><img src="../src/sprites/Machop.png" width={50} alt="cha" /></th>
+                    <th><img src="../src/sprites/Abra.png" width={50} alt="cha" /></th>
+                    <th><img src="../src/sprites/Gastly.png" width={50} alt="cha" />
                     </th>
 
                     <tr>
-                        <td><img src="../src/sprites/charizard.jpeg" width={50} alt="cha" /></td>
-                        <td><img src="../src/sprites/charizard.jpeg" width={50} alt="cha" /></td>
-                        <td><img src="../src/sprites/charizard.jpeg" width={50} alt="cha" /></td>
-                        <td><img src="../src/sprites/charizard.jpeg" width={50} alt="cha" /></td>
+                        <td><img src="../src/sprites/Magikarp.png" width={50} alt="cha" /></td>
+                        <td><img src="../src/sprites/Vulpix.png" width={50} alt="cha" /></td>
+                        <td><img src="../src/sprites/Oddish.png" width={50} alt="cha" /></td>
+                        <td><img src="../src/sprites/Zubat.png" width={50} alt="cha" /></td>
                 
                     </tr>
 
