e95848d6370c816091e4e2e8c2e53cfe59912735
João Victor M. V. <joaomiotelli17@gmail.com>
2025-10-27 23:38:10 -0300

    build(lint): adicionar ESLint, Prettier e plugins de acessibilidade

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,21 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2022: true, node: true, jest: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react/jsx-runtime',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  settings: { react: { version: 'detect' } },
+  plugins: ['react', 'react-hooks', 'jsx-a11y', 'react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
 }
