46c7fa51714ba8ab1272482f9a75bd1647178957
João Victor M. V. <joaomiotelli17@gmail.com>
2025-11-06 02:06:02 -0300

    feat(data): catálogo de Pokémon com stats/nível/imagens e ids estáveis

---
+++ src/shared/data/pokemonCatalog.js
@@ -0,0 +1,131 @@
+import AbraPng from '../sprites/Abra.png'
+import BulbasaurPng from '../sprites/bubasaur.png'
+import CharizardJpeg from '../sprites/charizard.jpeg'
+import EeveePng from '../sprites/Eevee.png'
+import GastlyPng from '../sprites/Gastly.png'
+import GeodudePng from '../sprites/Geodude.png'
+import MachopPng from '../sprites/Machop.png'
+import MagikarpPng from '../sprites/Magikarp.png'
+import MeowPng from '../sprites/meow.png'
+import OddishPng from '../sprites/Oddish.png'
+import PikachuPng from '../sprites/pikachu.png'
+import PsyduckPng from '../sprites/Psyduck.png'
+import SnorlaxPng from '../sprites/Snorlax.png'
+import SquirtlePng from '../sprites/Squirtle.png'
+import VulpixPng from '../sprites/Vulpix.png'
+import ZubatPng from '../sprites/Zubat.png'
+
+// Catálogo: fonte de verdade para image/stats/level
+export const catalog = [
+  {
+    species: 'Abra',
+    image: AbraPng,
+    level: 12,
+    stats: { hp: 25, atk: 20, def: 15, spA: 105, spD: 55, spe: 90 },
+  },
+
+  {
+    species: 'Bulbasaur',
+    image: BulbasaurPng,
+    level: 10,
+    stats: { hp: 45, atk: 49, def: 49, spA: 65, spD: 65, spe: 45 },
+  },
+
+  {
+    species: 'Charizard',
+    image: CharizardJpeg,
+    level: 36,
+    stats: { hp: 78, atk: 84, def: 78, spA: 109, spD: 85, spe: 100 },
+  },
+
+  {
+    species: 'Eevee',
+    image: EeveePng,
+    level: 10,
+    stats: { hp: 55, atk: 55, def: 50, spA: 45, spD: 65, spe: 55 },
+  },
+
+  {
+    species: 'Gastly',
+    image: GastlyPng,
+    level: 16,
+    stats: { hp: 30, atk: 35, def: 30, spA: 100, spD: 35, spe: 80 },
+  },
+
+  {
+    species: 'Geodude',
+    image: GeodudePng,
+    level: 12,
+    stats: { hp: 40, atk: 80, def: 100, spA: 30, spD: 30, spe: 20 },
+  },
+
+  {
+    species: 'Machop',
+    image: MachopPng,
+    level: 14,
+    stats: { hp: 70, atk: 80, def: 50, spA: 35, spD: 35, spe: 35 },
+  },
+
+  {
+    species: 'Magikarp',
+    image: MagikarpPng,
+    level: 5,
+    stats: { hp: 20, atk: 10, def: 55, spA: 15, spD: 20, spe: 80 },
+  },
+
+  {
+    species: 'Meowth',
+    image: MeowPng,
+    level: 11,
+    stats: { hp: 40, atk: 45, def: 35, spA: 40, spD: 40, spe: 90 },
+  },
+
+  {
+    species: 'Oddish',
+    image: OddishPng,
+    level: 12,
+    stats: { hp: 45, atk: 50, def: 55, spA: 75, spD: 65, spe: 30 },
+  },
+
+  {
+    species: 'Pikachu',
+    image: PikachuPng,
+    level: 12,
+    stats: { hp: 35, atk: 55, def: 40, spA: 50, spD: 50, spe: 90 },
+  },
+
+  {
+    species: 'Psyduck',
+    image: PsyduckPng,
+    level: 14,
+    stats: { hp: 50, atk: 52, def: 48, spA: 65, spD: 50, spe: 55 },
+  },
+
+  {
+    species: 'Snorlax',
+    image: SnorlaxPng,
+    level: 30,
+    stats: { hp: 160, atk: 110, def: 65, spA: 65, spD: 110, spe: 30 },
+  },
+
+  {
+    species: 'Squirtle',
+    image: SquirtlePng,
+    level: 10,
+    stats: { hp: 44, atk: 48, def: 65, spA: 50, spD: 64, spe: 43 },
+  },
+
+  {
+    species: 'Vulpix',
+    image: VulpixPng,
+    level: 13,
+    stats: { hp: 38, atk: 41, def: 40, spA: 50, spD: 65, spe: 65 },
+  },
+
+  {
+    species: 'Zubat',
+    image: ZubatPng,
+    level: 9,
+    stats: { hp: 40, atk: 45, def: 35, spA: 30, spD: 40, spe: 55 },
+  },
+]
